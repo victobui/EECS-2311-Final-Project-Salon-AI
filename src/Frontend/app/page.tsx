a75d504bde89afa88ea06c68faed28447cadd495
haadim1 <98914802+haadim1@users.noreply.github.com>
2025-03-07 06:23:42 -0500

    added Homepage planned homepage utilizing new components

---
+++ src/Frontend/app/page.tsx
@@ -0,0 +1,21 @@
+import FloatingNav from './components/FloatingNav';
+import HeroSection from './components/HeroSection';
+import FeaturesSection from './components/FeaturesSection';
+import TestimonialsSection from './components/TestimonialsSection';
+import CTASection from './components/CTASection';
+import Footer from './components/Footer';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <FloatingNav />
+      <main className="flex-grow">
+        <HeroSection />
+        <FeaturesSection />
+        <TestimonialsSection />
+        <CTASection />
+      </main>
+      <Footer />
+    </div>
+  );
+}
